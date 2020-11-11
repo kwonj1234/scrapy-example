7099a1f8384f0884ac3c9ac643567430517bda09
jaekwon <kwonj1234@gmail.com>
2020-11-11 16:57:31 -0500

    MVP

--- tutorial/tutorial/spiders/stocks_spiders.py
+++ tutorial/tutorial/spiders/stocks_spiders.py
@@ -9,33 +9,60 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         count = 0
         stocks_dict = {
-            "Market Movers" : {},
-            "Top Market Caps" : {},
-            "Top Dividens" : {}
+            "Market_Movers" : {},
+            "Top_Market_Caps" : {},
         }
-        for row in response.css('table.table').xpath('./tr'):
-            market_movers_list = []
-            market_caps = []
-            if 0 <= count < 10:
-                company_name = row.css('a::text').get()
-                # Price and time stamp are located inside a div with class 
-                # text-nowrap
-                nowrap_css = row.css('div.text-nowrap::text').getall()
-                price = nowrap_css[0]
-                time_stamp = nowrap_css[1]
-                # Percent change lives in a span within the text-nowrap div
-                # There are multiple spans within div.text-nowrap but we 
-                # know percent change is always the first within the row
-                # so we can use the .get() to retrieve the first instance
-                percent_change = row.css('div.text-nowrap')
-                                 .xpath('./span/text()').get()
+        # Scrape tables and tr tags on the page. tr tags are used to show
+        # data, but is also used for forms on this page so not all tr tags
+        # have relevant data
+        table_rows = response.css('table.table').xpath('./tr')
+        # All tr tags with children a tag have company names. There should
+        # be 25 names for each relevant table row
+        # 10 for market movers
+        # 10 for top market caps
+        # 10 for top dividents
+        company_names = table_rows.css('a::text').getall()
+        # Market Mover data
+        # rel_data contains both price and time stamp, the site uses the 
+        # same tag and class for both
+        rel_data = table_rows.css('div.text-nowrap::text').getall()
+        # change contains both percent change and actual change
+        change = table_rows.css('div.text-nowrap').xpath('./span/text()').getall()
+        # Top Market Cap data
+        cap = table_rows.css('td.text-right::text').getall()
+        # The rest of the rows contains another table called top 
+        # dividends, it is not as complicated as the other two so I 
+        # skipped it because if you can scrape the prior two tables,
+        # you most certainly scrape this one 
+        market_movers = {}
+        market_caps = {}
+        for i in range(len(company_names)):
+            if 0 <= i < 10:
+                price_index = percent_index = i * 2
+                time_index = change_index = price_index + 1
                 temp_dict = {
-                    "company name"  : company_name,
-                    "price"         : price,
-                    "time_stamp"    : time_stamp,
-                    "percent_change": percent_change
+                    "company_name"  : company_names[i],
+                    "price"         : rel_data[price_index],
+                    "time_stamp"    : rel_data[time_index],
+                    "percent_change": change[percent_index],
+                    "change"        : change[change_index]
                 }
-                market_movers_list.append(temp_dict)
-            if 10 <= count < 19:
-
-            
+                market_movers[i] = temp_dict
+            elif 10 <= i < 19:
+                cap_index = i - 10
+                temp_dict = {
+                    'company_name' : company_names[i],
+                    'cap'          : cap[cap_index]
+                }
+                market_caps[i] = temp_dict
+        # Store scraped data in main dictionary and store it
+        stocks_dict["Market_Movers"] = market_movers
+        stocks_dict["Top_Market_Caps"] = market_caps
+        # Take the time to write out the yield statement, scrapy does not
+        # format the JSON file nicely. All the data will be on one line
+        # doing this
+        # yield stocks_dict
+        yield {
+            "Market_Movers"  : market_movers,
+            "Top_Market_Caps": market_caps
+        }
